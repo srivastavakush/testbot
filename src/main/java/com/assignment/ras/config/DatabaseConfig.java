08cd2bfbf0641cbc2571d4fba6fc879972f2e9c4
Kush <kushprakash123@gmail.com>
2025-03-02 16:30:25 +0530

    data file

--- src/main/java/com/assignment/ras/config/DatabaseConfig.java
+++ src/main/java/com/assignment/ras/config/DatabaseConfig.java
@@ -1,17 +1,25 @@
 package com.assignment.ras.config;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import javax.sql.DataSource;
 
 @Configuration
-class DatabaseConfig {
-	@Bean
-	  public DataSource dataSource() {
+public class DatabaseConfig {
+    @Bean
+    public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSource.setUrl("jdbc:sqlserver://rasdb123.database.windows.net:1433;database=rasdb;user=CloudSA95467e5a@rasdb123;password=rasdb@123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+
+        String dbUrl = System.getenv("SPRING_DATASOURCE_URL");
+        String dbUsername = System.getenv("SPRING_DATASOURCE_USERNAME");
+        String dbPassword = System.getenv("SPRING_DATASOURCE_PASSWORD");
+
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+
         return dataSource;
     }
 }
